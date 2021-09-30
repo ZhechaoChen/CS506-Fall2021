8e00b91bd81c6e1c06059fa73c3e9df3618aae6e
Marstapeworm <zcchen@bu.edu>
2021-09-30 19:38:05 -0400

    update

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,7 +3,7 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    file = open(csv_file_path, "rw+")
+    file = open(csv_file_path, "r")
     lines=file.read(-1)
     lines=lines.split('\n')
     for row in range(len(lines)):
