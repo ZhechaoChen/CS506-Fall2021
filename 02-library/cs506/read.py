03403eff12d35252de17badafb98ffb1928ada20
Marstapeworm <zcchen@bu.edu>
2021-10-01 10:33:54 -0400

    update

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -13,6 +13,7 @@ def read_csv(csv_file_path):
                 lines[row][column]=(int) (lines[row][column])
             else:
                 lines[row][column]=lines[row][column][1:-2]
-    lines.remove([])
+    if len(lines[-1])==0
+        del lines[-1]
         
     return lines
